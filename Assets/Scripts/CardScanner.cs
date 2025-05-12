0ee1470e176b706b937bd522d04b9801730584e6
Juho Lauren <juholauren99@gmail.com>
2025-05-12 15:19:05 +0300

    card scanning part 2

--- Assets/Scripts/CardScanner.cs
+++ Assets/Scripts/CardScanner.cs
@@ -8,11 +8,12 @@ public class CardScanner : MonoBehaviour
     [Header("AR Session gameobject")]
     [SerializeField] private ARSession session;
     [Header("Image target testing")]
-    [SerializeField] private ImageTargetController cardTarget;
+    [SerializeField] private ImageTargetController[] cardTargetArray;
     [Header("Debug texts")]
     [SerializeField] private TMP_Text targetInfo;
     [Header("Other")]
     public UnityEvent<CardSO> onCardScanned;
+    [SerializeField] private GameObject scanningPanel;
     private ImageTrackerFrameFilter imageTracker;
     private CameraDeviceFrameSource cameraDevice;
     private bool isScanning;
@@ -21,15 +22,21 @@ public class CardScanner : MonoBehaviour
     {
         imageTracker = session.GetComponentInChildren<ImageTrackerFrameFilter>();
         cameraDevice = session.GetComponentInChildren<CameraDeviceFrameSource>();
-        AddTargetControllerEvents(cardTarget);
+        scanningPanel.SetActive(false);
+        foreach(var card in cardTargetArray)
+        {
+            AddTargetControllerEvents(card);
+        }
     }
     public void EnableScanning()
     {
         isScanning = true;
+        scanningPanel.SetActive(true);
     }
     public void DisableScanning()
     {
         isScanning = false;
+        scanningPanel.SetActive(false);
     }
     private void AddTargetControllerEvents(ImageTargetController controller)
     {
